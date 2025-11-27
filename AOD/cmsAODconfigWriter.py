71576d6ef30a6e6ffaf358c937f9a3223e5135b7
crovelli <chiara.rovelli@cern.ch>
2025-11-27 17:51:02 +0100

    first commit

---
+++ AOD/cmsAODconfigWriter.py
@@ -0,0 +1,91 @@
+import argparse
+
+import sys
+import os
+sys.path.append( os.path.join( os.path.dirname(os.path.abspath(__file__)), os.pardir))
+import config.conditions as conditions
+
+def cmsDriver_command(filein = None, campaign = 'Run3Summer22', number = 1000) :
+    """
+    Generate the cmsDriver command for the given conditions
+    based on the campaign specified in the command line argument.
+    = produce the config file for cmsRun 
+    """
+    
+    if filein is None:
+        raise ValueError("Input file must be specified.")
+        return
+    #if not os.path.exists(genfragment):
+    #    raise FileNotFoundError(f"Gen fragment '{genfragment}' does not exist.")
+    #    return
+    file_base = os.path.basename(filein)
+    if file_base.endswith('DRPremix.root'):
+        file_base = file_base[:-len('DRPremix.root')] 
+    out_config = f'{file_base}AOD_cfg.py'
+    out_root = f'{file_base}AOD.root'   
+
+
+    if campaign not in conditions.conditions:
+        raise ValueError(f"Campaign '{campaign}' not found in conditions.")
+    cond = conditions.conditions[campaign]
+    
+    command = " ".join(['cmsDriver.py',
+                        '--eventcontent AODSIM',
+                        '--datatier AODSIM',
+                        '--conditions', cond['globalTag'],
+                        '--step RAW2DIGI,L1Reco,RECO,RECOSIM',
+                        '--beamspot', cond['beamSpot'],
+                        '--procModifiers siPixelQualityRawToDigi',
+                        '--geometry DB:Extended',
+                        '--era', cond['era'],
+                        '--python_filename', out_config,
+                        '--fileout', f'file:{out_root}',
+                        '--filein', f'file:{os.path.basename(filein)}',
+                        '--mc',
+                        '--no_exec',
+                        '--number', str(number),
+                        '--number_out', str(number),
+    ])
+    print(command + '\n')
+
+    return command, out_config
+
+
+if __name__ == "__main__":
+
+    argparser = argparse.ArgumentParser(description="Generate cmsDriver command for a given campaign.")
+    argparser.add_argument("--filein", 
+                           default=None, 
+                           help="Input file from DRPremix stage to use as input for the AOD step.")
+    argparser.add_argument("--campaign", 
+                           choices=conditions.campaigns,
+                           default="Run3Summer22", 
+                           help="Campaign to use")
+    argparser.add_argument("-N", "--number",
+                           type=int, default=100, 
+                           help="Number of events to produce (default: 100)")
+    argparser.add_argument("--dryrun",
+                           action="store_true", 
+                           help="If set, only print the command without executing it")
+    args = argparser.parse_args()
+    print('\n')
+
+    filein = args.filein
+    campaign = args.campaign
+    N = args.number
+    cmd, cfg = cmsDriver_command(filein, campaign, N)
+    
+    if args.dryrun:
+        print("[DRY-RUN] command not executed.")
+        exit(0)
+    print("[EXE] executing command...")
+    os.system(cmd)
+    if os.path.exists(cfg):
+        print(f" DONE - config file {cfg} created.")
+        os.system(f' mv {cfg} test/')
+    else:
+        print(f"[ERROR] config file {cfg} not created.")
+        exit(1)
+       
+    
+
---
+++ AOD/production/crabconfig.py
@@ -0,0 +1,107 @@
+import CRABClient
+from CRABClient.UserUtilities import config, ClientException
+import yaml
+import datetime
+from fnmatch import fnmatch
+import argparse
+
+import sys
+import os
+sys.path.append( os.path.join( os.path.dirname(os.path.abspath(__file__)), os.pardir, os.pardir))
+import config.conditions as conditions
+
+if __name__ == '__main__':
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from http.client import HTTPException
+    from multiprocessing import Process
+
+    parser = argparse.ArgumentParser(description='CRAB configuration for tau3mu production')
+    parser.add_argument('--config',
+                        type=str, required=True,
+                        help='path to the configuration file to submit on CRAB')
+    parser.add_argument('--input_dataset',
+                        type=str, required=True,
+                        help='input dataset from GS stage to use - DAS string')
+    parser.add_argument('--process_name',
+                        type=str, default='WtoTauNu_Tauto3Mu',
+                        help='process to simulate - default is tau3mu')
+    parser.add_argument('--campaign',
+                        choices= conditions.campaigns, default='Run3Summer22',
+                        help='campaign for the production')
+    parser.add_argument('-N', '--Nevents',
+                        type=int, default=50000,
+                        help='number of events to produce')
+    parser.add_argument('-t', '--tag',
+                        type=str, default='MTau1950',
+                        help='tag for the production - use MTauxxxx for the mass of the tau in MeV')
+    parser.add_argument('-v', '--version',
+                        type=str, default='v0',
+                        help='Version of the production')
+    parser.add_argument('--dryrun',
+                        action='store_true',
+                        help='perform a dry run without submitting the task')
+    args = parser.parse_args()
+
+    dry_run_ = args.dryrun
+    
+    config_file     = args.config
+
+    process_name    = args.process_name
+    tag             = args.tag
+    campaign        = args.campaign
+    step            = 'AOD' # RAW2DIGI,L1Reco,RECO,RECOSIM
+    production_tag  = datetime.date.today().strftime('%Y%b%d')
+    version         = 'v'+str(args.version)
+
+    input_dataset   = args.input_dataset
+
+    request_name    = '_'.join([process_name, tag, campaign+step, version])
+    work_area       = '_'.join([process_name, campaign+step, version, production_tag]) 
+    dataset_tag     = '_'.join([process_name, tag, campaign+step, version])
+    dataset_name    = '_'.join([process_name, tag, 'TuneCP5_13p6TeV_pythia8', campaign+step, version])
+
+    config = config()
+
+    config.section_('General')
+    config.General.requestName = request_name
+    config.General.workArea = work_area 
+    config.General.transferOutputs = True
+    config.General.transferLogs = True
+
+    config.section_('Data')
+    config.Data.publication = True
+    config.Data.outputDatasetTag = dataset_tag
+    #config.Data.outputPrimaryDataset = dataset_name 
+    config.Data.outLFNDirBase = '/store/group/phys_bphys/crovelli/%s' % (config.General.workArea)
+    # DBS is 'global' for public productions, 'phys03' for private productions
+    config.Data.inputDBS = 'phys03' # 'global'
+    config.Data.inputDataset = input_dataset
+    config.Data.splitting = 'FileBased'
+    config.Data.unitsPerJob = 1
+
+    config.section_('JobType')
+    config.JobType.pluginName = 'Analysis'
+    config.JobType.psetName = config_file
+    config.JobType.inputFiles = []
+    config.JobType.disableAutomaticOutputCollection = False # automatic recognition of output files
+    #config.JobType.outputFiles = ['ppW3MuNu_Run3Summer22EEwmLHEGS.root']
+    config.JobType.allowUndistributedCMSSW = True
+    config.JobType.maxMemoryMB = 4000 # 2500 (default) # fixme: check if this is needed
+
+    config.section_('User')
+
+    config.section_('Site')
+    config.Site.storageSite = 'T2_CH_CERN'
+ 
+    def submit(config):
+      try:
+          print(config)
+          if not dry_run_: crabCommand('submit', config = config)
+      except HTTPException as hte:
+          print("Failed submitting task:",hte.headers)
+      except ClientException as cle:
+          print("Failed submitting task:",cle)
+
+
+    submit(config)
